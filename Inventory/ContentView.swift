703c2562a4c3c077be75cc2ddeecc3ce79db3ece
Merouane Bellaha <m.bellaha@gmail.com>
2020-09-18 09:11:44 +0200

    Initial Commit

---
+++ Inventory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inventory
+//
+//  Created by Merouane Bellaha on 18/09/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
